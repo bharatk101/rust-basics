414277c5e06bfae41fa170a277d547eed429dd76
bharatk101 <kulkarnibharat018@gmail.com>
2022-05-06 16:06:01 +0530

    Completed arrays and tuples'

---
+++ src/arrays.rs
@@ -0,0 +1,11 @@
+//  Array's are fixed size elements of same data type
+
+pub fn run() {
+    let numbers: [i32; 5] = [1, 2, 3, 4, 5]; // data type and the size
+
+    println!("{:?}", numbers);
+
+    //  get single value
+    println!("{}", numbers[0]);
+    
+}
